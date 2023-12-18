f8e9d284e765ae4144706c26b9ccf0025ad67ac4
vladpapko <vlad.papkoo@mail.ru>
2023-12-18 22:26:49 +0700

    two commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    private const RECENT_ARTICLE_COUNT_0N_HOME = 5;
+    #[Route('/', name: 'homepage')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $articleRepository->getRecentArticles(self::RECENT_ARTICLE_COUNT_0N_HOME),
+        ]);
+    }
+}
---
+++ src/EntityListener/ArticleEntityListener.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EntityListener;
+
+use App\Entity\Article;
+use App\Message\ArticleNotification;
+use App\MessageHandler\ArticleMessageHandler;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: Article::class)]
+#[AsEntityListener(event: Events::preUpdate, method: 'preUpdate', entity: Article::class)]
+#[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: Article::class)]
+class ArticleEntityListener
+{
+    public function __construct(
+        private readonly Security $security,
+        private readonly MessageBusInterface $bus
+    )
+    {
+    }
+
+    public function prePersist(\App\Entity\Article $entity): void
+    {
+        $entity->setCreatedAt(new \DateTimeImmutable())
+            ->setAuthor($this->security->getUser());
+    }
+
+    public function postPersist(\App\Entity\Article $entity)
+    {
+        $this->bus->dispatch(new ArticleNotification(
+            $entity->getId(),
+            $entity->getTitle(),
+            $entity->getBody(),
+            $entity->getAuthor()->getId(),
+            true,
+        ));
+    }
+
+    public function preUpdate(\App\Entity\Article $entity, PreUpdateEventArgs $event): void
+    {
+        $this->bus->dispatch(new ArticleNotification(
+            $entity->getId(),
+            $entity->getTitle(),
+            $entity->getBody(),
+            $entity->getAuthor()->getId(),
+            false,
+        ));
+    }
+}
